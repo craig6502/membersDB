1bd513d0d992114bf46ff4aefe5c4151c81c09b6
Craig Duncan <47374595+craig6502@users.noreply.github.com>
2019-02-06 20:17:34 +0800

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: select * from southperth.members
+LIMIT 0, 1000
+
+-- Date: 2017-08-10 17:01
+*/
+INSERT INTO `members` (`club_id`,`mycricket_id`,`last_name`,`first_name`,`Address1`,`Suburb`,`Postcode`,`phone1`,`parents`,`mob1`,`mob2`,`parentsemail`,`mf`,`birthdate`,`yn`,`school`,`age`,`notes`) VALUES (1,0,'Smith','John','98 Grt Eastern Street','Port Norris','8151','08 9974 1234','Janet & Eric','F:0123 456 789','M:0123 456 789','thesmiths@hotmail.com','m','24/2/10','','Port Primary','10','notes');
